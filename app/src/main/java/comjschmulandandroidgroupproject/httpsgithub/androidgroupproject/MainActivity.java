fd4d51c47bc2b6a7b10b34f2b2be7fcfcee96231
jschmuland <joelschmuland@gmail.com>
2017-03-09 13:11:19 -0500

    Buttons and text added to main page

--- app/src/main/java/comjschmulandandroidgroupproject/httpsgithub/androidgroupproject/MainActivity.java
+++ app/src/main/java/comjschmulandandroidgroupproject/httpsgithub/androidgroupproject/MainActivity.java
@@ -1,7 +1,12 @@
 package comjschmulandandroidgroupproject.httpsgithub.androidgroupproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,55 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ProgressBar calorieBar = (ProgressBar)findViewById(R.id.progressBar);
+        calorieBar.setProgress(69);
+
+        View sleepbtn = findViewById(R.id.mainSleepBtn);
+        sleepbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("StartActivity","User cliked Weather Button");
+                Intent intent = new Intent(MainActivity.this, SleepTracker.class);
+                startActivity(intent);
+            }
+        });
+
+        View excercisebtn = findViewById(R.id.mainExcerciseBtn);
+        excercisebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("StartActivity","User cliked Weather Button");
+                Intent intent = new Intent(MainActivity.this, SleepTracker.class);
+                startActivity(intent);
+            }
+        });
+
+        View foodbtn = findViewById(R.id.mainFoodBtn);
+        foodbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("StartActivity","User cliked Weather Button");
+                Intent intent = new Intent(MainActivity.this, SleepTracker.class);
+                startActivity(intent);
+            }
+        });
+
+        View mealbtn = findViewById(R.id.mainMealBtn);
+        mealbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("StartActivity","User cliked Weather Button");
+                Intent intent = new Intent(MainActivity.this, SleepTracker.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 }
