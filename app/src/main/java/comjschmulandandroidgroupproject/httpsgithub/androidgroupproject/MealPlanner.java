ebb6d87f4b3cd1e7e9f40933f36c19e4d7daddd5
kmcnulty <mcnu0067@algonquinlive.com>
2017-03-26 17:03:20 -0400

    Created MealPlanner

---
+++ app/src/main/java/comjschmulandandroidgroupproject/httpsgithub/androidgroupproject/MealPlanner.java
@@ -0,0 +1,30 @@
+package comjschmulandandroidgroupproject.httpsgithub.androidgroupproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+
+
+public class MealPlanner extends AppCompatActivity {
+
+   ListView theList= (ListView)findViewById(R.id.theList);
+    Button button=(Button)findViewById(R.id.mealsubmitbutton);
+    EditText editText=(EditText)findViewById(R.id.editText);
+    ProgressBar progressBar=(ProgressBar)findViewById(R.id.progressBar);
+}
