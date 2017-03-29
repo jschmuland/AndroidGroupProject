99a024cb2e9d63c26dea94966d9fbdd3d88fe88f
James-Thibaudeau <jamesthibaudeau@gmail.com>
2017-03-29 10:30:19 -0400

    Fixed up models, added FoodTracker activity to manifest, added string for FoodTracker to strings.xml

---
+++ app/src/main/java/comjschmulandandroidgroupproject/httpsgithub/androidgroupproject/models/Meal.java
@@ -0,0 +1,68 @@
+package comjschmulandandroidgroupproject.httpsgithub.androidgroupproject.models;
+
+import java.util.ArrayList;
+
+public class Meal {
+    int id;
+    String mealName;
+    ArrayList<Food> foods;
+
+    public Meal(){
+        this.mealName = "";
+        this.foods = new ArrayList<>();
+        this.id = 0;
+    }
+
+    public Meal(String mealName) {
+        this();
+        this.mealName = mealName;
+    }
+
+    public Meal(String mealName, ArrayList<Food> foods){
+        this(mealName);
+        this.foods = foods;
+    }
+
+    public Meal(int id, String mealName, ArrayList<Food> foods){
+        this(mealName, foods);
+        this.id = id;
+    }
+
+    public void addFood(Food food){
+        foods.add(food);
+    }
+
+    public void removeFood(Food food) {
+        if(foods.contains(food)){
+            foods.remove(food);
+        }
+    }
+
+    public Food getFood(int index){
+        return foods.get(index);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public ArrayList<Food> getFoods() {
+        return foods;
+    }
+
+    public void setFoods(ArrayList<Food> foods) {
+        this.foods = foods;
+    }
+
+    public String getMealName() {
+        return mealName;
+    }
+
+    public void setMealName(String mealName) {
+        this.mealName = mealName;
+    }
+}
