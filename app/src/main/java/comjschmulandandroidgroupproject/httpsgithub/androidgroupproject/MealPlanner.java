eae91a89816d71985d5802361189759f853b2138
kmcnulty <mcnu0067@algonquinlive.com>
2017-04-01 10:56:50 -0400

    got async task working and added internet permission

--- app/src/main/java/comjschmulandandroidgroupproject/httpsgithub/androidgroupproject/MealPlanner.java
+++ app/src/main/java/comjschmulandandroidgroupproject/httpsgithub/androidgroupproject/MealPlanner.java
@@ -1,9 +1,15 @@
 package comjschmulandandroidgroupproject.httpsgithub.androidgroupproject;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.JsonReader;
 import android.util.Log;
+import android.util.Xml;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,11 +19,22 @@ import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.xmlpull.v1.XmlPullParser;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 
@@ -32,7 +49,104 @@ public class MealPlanner extends AppCompatActivity {
         Button button=(Button)findViewById(R.id.mealsubmitbutton);
         EditText editText=(EditText)findViewById(R.id.editText);
         ProgressBar progressBar=(ProgressBar)findViewById(R.id.progressBar);
+        //
+        try{
+            NutritionQuery thread =
+                    new NutritionQuery("https://service.livestrong.com/service/food/foods/?query=mango");
+            thread.execute();
+        }catch(Exception e){
+            Log.d("errorforecast", e.toString());
+        }
 
+    }
+
+    private class NutritionQuery extends AsyncTask<String, Integer, String[]>
+    {
+        String min;
+        String max;
+
+        String current;
+        Bitmap image;
+        ProgressBar progressBar;
+
+        URL url, imageURL;
+        String iconVal;
+        String in = "in";
+        public NutritionQuery(String url){
+            progressBar=(ProgressBar)findViewById(R.id.progressBar);
+            progressBar.setMax(100);
+            try{
+                this.url = new URL(url);
+            }catch(MalformedURLException e){
+                e.printStackTrace();
+            }
+        }
+        @Override
+        protected String[] doInBackground(String ... args)
+        {
+            String[] entries = new String[4];
+            boolean skiptheRest = false;
+            try {
+
+                HttpURLConnection conn = null;
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestProperty("Accept-Encoding", "");
+                conn.setReadTimeout(100000);
+                conn.setConnectTimeout(150000);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+                conn.connect();
+
+                JsonReader reader = new JsonReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+                reader.beginObject();
+                while (reader.hasNext()) {
+                    String name = reader.nextName();
+                    if(name.equals("query")){
+                        Log.d("MEALPLANNER-API-QUERY", reader.nextString());
+                    }else if(name.equals("foods")){
+                        reader.beginArray();
+                        while(reader.hasNext()){
+                            reader.beginObject();
+                            while(reader.hasNext()){
+                                String currObj = reader.nextName();
+                                if(currObj.equals("cals") && !skiptheRest){
+                                    skiptheRest = true;
+                                    double calories = reader.nextDouble();
+                                    Log.d("MEALPLANNE-API-CALORIES", calories+"");
+                                }else{
+
+                                    reader.skipValue();
+
+                                }
+                            }
+                            reader.endObject();
+                        }
+                        reader.endArray();
+                    }else{
+                        reader.skipValue();
+
+                    }
+
+                }
+                reader.endObject();
+                publishProgress(100);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return entries;
+        }
+        @Override
+        protected void onProgressUpdate(Integer... prog){
+            progressBar.setProgress(prog[0]);
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        public void onPostExecute(String[] res){
+            super.onPostExecute(res);
+            progressBar.setVisibility(View.INVISIBLE);
+        }
 
     }
 }
