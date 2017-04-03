26d95c3498ac97a9b39034fffdf670ad11092ff0
James-Thibaudeau <jamesthibaudeau@gmail.com>
2017-04-03 17:34:43 -0400

    Added a snackbar confirmation for adding food

--- app/src/main/java/comjschmulandandroidgroupproject/httpsgithub/androidgroupproject/FoodTracker.java
+++ app/src/main/java/comjschmulandandroidgroupproject/httpsgithub/androidgroupproject/FoodTracker.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -74,8 +75,8 @@ public class FoodTracker extends AppCompatActivity {
                 }
                 foodObjArr = dbHelper.getAllFoodEaten();
                 foodAdapter.notifyDataSetChanged();
-
-                makeToast(R.string.successToast);
+                
+                makeSnackBar(R.string.successToast);
             }
         });
 
@@ -139,6 +140,11 @@ public class FoodTracker extends AppCompatActivity {
             return false;
         }
         return true;
+    }
+
+    private void makeSnackBar(int message){
+        Snackbar snack = Snackbar.make(findViewById(android.R.id.content), getText(message).toString(), Snackbar.LENGTH_SHORT);
+        snack.show();
     }
 
     private void makeToast(int message) {
